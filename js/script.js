04a6ce8c9e3cd1fb04a3108d9bb60a2f10e34372
AnnaBudda <ffforza@yandex.ru>
2017-09-08 00:16:10 +0300

    JS

--- js/script.js
+++ js/script.js
@@ -1,11 +1,7 @@
-var popup = document.querySelector(".button-popup-search")
-          popupform = document.querySelector(".popup-form");
+var popup = document.querySelector(".button-popup-search");
+var popupform = document.querySelector(".popup-form");
 
-  popup.addEventListener("click", function() {
-        popupform.classList.toggle("popup-form-active");
-});
-
-  searchButton.addEventListener('click', function (evt) {
-      document.getElementsByTagName("form")[0].classList.add("form-show");
-        document.getElementsByTagName("form")[0].style.visibility = "visible";
+popup.addEventListener("click", function() {
+  evt.preventDefault();
+  popupform.classList.toggle("popup-form-active");
 });
