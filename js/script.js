5d50172cdc20e3cb6823fabafb57d1df611be25c
AnnaBudda <ffforza@yandex.ru>
2017-09-08 11:51:24 +0300

    Name

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 var popup = document.querySelector(".button-popup-search");
 var popupform = document.querySelector(".popup-form");
 
-popup.addEventListener("click", function(evt) {
+popup.addEventListener("click", function (evt) {
   evt.preventDefault();
   popupform.classList.toggle("popup-form-active");
 });
