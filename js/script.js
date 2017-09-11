ae6ae244cf3f85f8c9bcfaa666e43f5ce6574883
AnnaBudda <ffforza@yandex.ru>
2017-09-11 21:02:15 +0300

    Финал 4

--- js/script.js
+++ js/script.js
@@ -3,16 +3,21 @@ var popupform = document.querySelector(".popup-form");
 var dateIn = popupform.querySelector("[name=date-in]");
 var dateOut = popupform.querySelector("[name=date-out]");
 var amountAdult = popupform.querySelector("[name=adults]");
+var amountChild = popupform.querySelector("[name=children]");
+var btnSubmit = document.querySelector(".popup-search-btn");
 
 popup.addEventListener("click", function (evt) {
   evt.preventDefault();
   popupform.classList.toggle("popup-form-hidden");
+  popupform.classList.remove("popup-return");
 });
 
 popupform.addEventListener("submit", function(evt){
   if (!dateIn.value || !dateOut.value || !amountAdult.value){
     evt.preventDefault();
-    popupform.classList.add("popup-return");
+    popupform.classList.remove("popup-return");
+    popupform.offsetWidth = popupform.offsetWidth;
     console.log("Введите данные");
+    popupform.classList.add("popup-return");
   }
 });
